18d0231a92b5b124e5aa658581d978a9ad87345e
lmitchell4 <lmitchell4@gmail.com>
2018-06-09 20:51:45 -0700

    Clear out old site

--- js/profile_js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  document.addEventListener("scroll", function() {
-    if(window.pageYOffset > 0) {
-      $("#header").addClass("header-shadow");
-    } else {
-      $("#header").removeClass("header-shadow");
-    }
-  });
-})
